9d60108aa5710a7445a78d2e1889abd36466b466
saranafisa <sara_nafisa@protonmail.com>
2023-03-20 09:58:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+let employee = {
+    name: "Sara",
+    streetAddress: "Gaziantep, Turkey",
+  };
+  
+
+ function updateEmployeeWithKeyAndValue(employee, key, value){
+    const changedEmployee = { ...employee };
+    changedEmployee[key] = value;
+  return changedEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+   return employee;
+
+  }
+  function deleteFromEmployeeByKey(employee, key){
+
+    let changedEmployee = { ...employee };
+    delete changedEmployee[key];
+    return changedEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+    delete employee[key];
+    return employee;
+  }
